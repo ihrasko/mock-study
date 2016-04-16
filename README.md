870735b3ef4146aa3fd3ff14c85611dce77a329d
Ivan Hrasko <abhrasko@gmail.com>
2016-04-17 01:42:07 +0200

    added SLF4J support

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ https://en.wikipedia.org/wiki/Behavior-driven_development
 
 http://www.codeproject.com/Articles/516360/Mockito-a-great-mock-framework-for-Java-developmen
 
+http://www.captaindebug.com/2011/09/adding-slf4j-to-your-maven-project.html#.VxLLYHWLTVM
+
 http://www.megster.net/2014/unit-testing-log4j-method-calls-with-junit-and-mockito
 
 https://dzone.com/articles/unit-testing-asserting-line
--- src/main/java/Network.java
+++ src/main/java/Network.java
@@ -1,15 +1,21 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Network class
  */
 public class Network {
 
+    private static Logger logger = LoggerFactory.getLogger(Network.class);
+
     private List<Computer> computers;
 
     public Network() {
+        logger.info("Creating Network");
+
         if (this.computers == null) {
             this.computers = new ArrayList<Computer>();
         }
